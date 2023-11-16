c96e9501198298a36123f3831dc99c8578eb262d
Sena Lim <tpsk8322@naver.com>
2023-11-16 16:55:11 +0900

    fix: 로그인한 경우 Header에 사용자 닉네임/이름 표시

--- src/components/base/Header.js
+++ src/components/base/Header.js
@@ -1,8 +1,11 @@
-import styles from "./header.module.css";
 import {Link} from "react-router-dom";
 import {BiSolidLocationPlus} from "react-icons/bi";
+import styles from "./header.module.css";
 
 const Header = () => {
+    const member = JSON.parse(localStorage.getItem('member'));
+    const staff = JSON.parse(localStorage.getItem('staff'));
+
     return (
         <div className={styles.header}>
             <Link to="/" className={styles.logo}>
@@ -14,7 +17,21 @@ const Header = () => {
                 <Link to='/reviews'>병원 리뷰</Link>
                 <Link to='/boards'>질문 게시판</Link>
                 <Link to='/health-infos'>건강정보</Link>
-                <Link to='/auth/login/member' className={styles.login}>로그인</Link>
+                {member &&
+                    <Link to=''>
+                        <span className={styles.name}>{member.nickname}</span>님
+                    </Link>
+                }
+                {staff &&
+                    <Link to=''>
+                        <span className={styles.name}>{staff.name}님</span>
+                    </Link>
+                }
+                {!member && !staff &&
+                    <Link to='/auth/login/member' className={styles.login}>
+                        로그인
+                    </Link>
+                }
             </nav>
         </div>
     );
