86cccc2bc403e5ec8b075897d1b5071d2271ff95
Sena Lim <tpsk8322@naver.com>
2023-11-19 19:01:31 +0900

    style: 게시글 상세 조회 페이지 스타일링

--- src/components/board/BoardDetail.js
+++ src/components/board/BoardDetail.js
@@ -1,26 +1,30 @@
 import {IoPersonCircleOutline} from "react-icons/io5";
+import styles from "./board-detail.module.css";
+import {FaRegComments} from "react-icons/fa";
 
 const BoardDetail = ({board, boardError}) => {
     if (boardError) return <div>게시글 상세 조회 실패</div>
 
-    const {title, body, dept, writer, createAt} = board.data;
+    const {title, body, dept, writer, createAt, boardReplyList} = board.data;
 
     return (
-        <div>
-            <div>
-                <div>{dept}</div>
-                <div>{title}</div>
-                <div>
-                    <div><IoPersonCircleOutline/></div>
-                    <div>
-                        <div>{writer}</div>
-                        <div>{createAt}</div>
+        <div className={styles.boxBoardDetail}>
+            <div className={styles.boxSubInfo}>
+                <div className={styles.deptTag}>{dept}</div>
+                <h1 className={styles.boardTitle}>{title}</h1>
+                <div className={styles.boxSubInfo2}>
+                    <div className={styles.writerIcon}><IoPersonCircleOutline/></div>
+                    <div className={styles.boxWriteInfo}>
+                        <span className={styles.boardWriter}>{writer}</span>
+                        <span className={styles.boardWriteTime}>{createAt}</span>
+                    </div>
+                    <div className={styles.boxReplyInfo}>
+                        <span className={styles.replyIcon}><FaRegComments/></span>
+                        <span>{boardReplyList.length}개의 댓글</span>
                     </div>
                 </div>
             </div>
-            <div>
-                {body}
-            </div>
+            <div className={styles.boxBoardBody} dangerouslySetInnerHTML={{__html: body}}/>
         </div>
     );
 };
